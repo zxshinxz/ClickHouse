9f10597bb57946c5c5ba56d896e80a71ef978e3f
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2018-09-26 16:58:42 +0300

    Merging with master (in progress).

--- dbms/src/Parsers/ASTAlterQuery.cpp
+++ dbms/src/Parsers/ASTAlterQuery.cpp
@@ -162,7 +162,7 @@ void ASTAlterCommand::formatImpl(
         settings.ostr << (settings.hilite ? hilite_keyword : "") << " WHERE " << (settings.hilite ? hilite_none : "");
         predicate->formatImpl(settings, state, frame);
     }
-    else if (p.type == ASTAlterQuery::ADD_TO_PARAMETER)
+    else if (p.type == ASTAlterCommand::ADD_TO_PARAMETER)
     {
         settings.ostr << (settings.hilite ? hilite_keyword : "") << indent_str << "ADD TO PARAMETER " << (settings.hilite ? hilite_none : "");
         p.parameter->formatImpl(settings, state, frame);
@@ -171,7 +171,7 @@ void ASTAlterCommand::formatImpl(
 
         p.values->formatImpl(settings, state, frame);
     }
-    else if (p.type == ASTAlterQuery::DROP_FROM_PARAMETER)
+    else if (p.type == ASTAlterCommand::DROP_FROM_PARAMETER)
     {
         settings.ostr << (settings.hilite ? hilite_keyword : "") << indent_str << "DROP FROM PARAMETER " << (settings.hilite ? hilite_none : "");
         p.parameter->formatImpl(settings, state, frame);
@@ -180,7 +180,7 @@ void ASTAlterCommand::formatImpl(
 
         p.values->formatImpl(settings, state, frame);
     }
-    else if (p.type == ASTAlterQuery::MODIFY_PARAMETER)
+    else if (p.type == ASTAlterCommand::MODIFY_PARAMETER)
     {
         settings.ostr << (settings.hilite ? hilite_keyword : "") << indent_str << "MODIFY PARAMETER " << (settings.hilite ? hilite_none : "");
         p.parameter->formatImpl(settings, state, frame);
@@ -189,37 +189,37 @@ void ASTAlterCommand::formatImpl(
 
         p.values->formatImpl(settings, state, frame);
     }
-    else if (p.type == ASTAlterQuery::CHANNEL_ADD)
+    else if (p.type == ASTAlterCommand::CHANNEL_ADD)
     {
         settings.ostr << (settings.hilite ? hilite_keyword : "") << indent_str << "ADD " << (settings.hilite ? hilite_none : "");
 
         p.values->formatImpl(settings, state, frame);
     }
-    else if (p.type == ASTAlterQuery::CHANNEL_DROP)
+    else if (p.type == ASTAlterCommand::CHANNEL_DROP)
     {
         settings.ostr << (settings.hilite ? hilite_keyword : "") << indent_str << "DROP " << (settings.hilite ? hilite_none : "");
 
         p.values->formatImpl(settings, state, frame);
     }
-    else if (p.type == ASTAlterQuery::CHANNEL_MODIFY)
+    else if (p.type == ASTAlterCommand::CHANNEL_MODIFY)
     {
         settings.ostr << (settings.hilite ? hilite_keyword : "") << indent_str << "MODIFY " << (settings.hilite ? hilite_none : "");
 
         p.values->formatImpl(settings, state, frame);
     }
-    else if (p.type == ASTAlterQuery::CHANNEL_SUSPEND)
+    else if (p.type == ASTAlterCommand::CHANNEL_SUSPEND)
     {
         settings.ostr << (settings.hilite ? hilite_keyword : "") << indent_str << "SUSPEND " << (settings.hilite ? hilite_none : "");
 
         p.values->formatImpl(settings, state, frame);
     }
-    else if (p.type == ASTAlterQuery::CHANNEL_RESUME)
+    else if (p.type == ASTAlterCommand::CHANNEL_RESUME)
     {
         settings.ostr << (settings.hilite ? hilite_keyword : "") << indent_str << "RESUME " << (settings.hilite ? hilite_none : "");
 
         p.values->formatImpl(settings, state, frame);
     }
-    else if (p.type == ASTAlterQuery::CHANNEL_REFRESH)
+    else if (p.type == ASTAlterCommand::CHANNEL_REFRESH)
     {
         settings.ostr << (settings.hilite ? hilite_keyword : "") << indent_str << "REFRESH " << (settings.hilite ? hilite_none : "");
 
